7ee6d52a52cecf60698ba77d03efafc22f916768
raviteja-venkata-kilaru <kilaruvenkata.raviteja@randstaddigital.com>
2024-05-20 19:49:50 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'VenkataRavitejaKilaru'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
 db = SQLAlchemy(app)
 
@@ -19,33 +20,47 @@ class Order(db.Model):
 @app.route('/api/products/<int:id>', methods=['GET'])
 def get_product(id):
     product = Product.query.get_or_404(id)
-    return jsonify({'id': product.id, 'name': product.name, 'price': product.price})
+    return jsonify({'id': product.id, 'name': product.name, 'price': product.price,'stock': product.stock})
 
-@app.route('/api/allproducts', methods=['GET'])
+@app.route('/', methods=['GET'])
 def All_products():
     product = Product.query.all()
-    print(product)
     return render_template('index.html',product = product), 200
 
-@app.route('/add_product')
-def add_product():
-    return render_template('Create_Product.html')
 
-@app.route('/api/createProducts', methods=['POST'])
+@app.route('/api/createProducts', methods=['GET','POST'])
 def create_product():
-    data = request.json
-    product = Product(name=data['name'], price=data['price'],stock=data['stock'])
-    db.session.add(product)
-    db.session.commit()
-    return jsonify({'message': 'Product saved successfully'}),201
+    if request.method == 'POST':
+        data = request.json
+        product = Product.query.filter_by(name=data['name']).first()
+        if product:
+            product.price=float(data['price'])
+            product.stock+=int(data['stock'])
+            db.session.commit()
+            return jsonify({'message': 'Product Updated successfully'}),201
+        product = Product(name=data['name'], price=float(data['price']),stock=int(data['stock']))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({'message': 'Product saved successfully'}),201
+    return render_template('Create_Product.html')
 
 @app.route('/api/order_products', methods=['GET','POST'])
 def Order_product():
     if request.method == 'POST':
-        name = request.form['name']
-        quantity = int(request.form['quantity'])
-        order = Order(name = name, quantity = quantity)
-        return jsonify({"message":"Your Order is places"}), 200
+        data = request.json
+        name=data['name']
+        quantity = int(data['quantity'])
+        product = Product.query.filter_by(name=name).first()
+        if product:
+            if quantity <= product.stock:
+                order = Order(name = name, quantity = quantity)
+                db.session.add(order)
+                db.session.commit()
+                product.stock-=quantity
+                db.session.commit()
+                return jsonify({"message" : "Your Order is places"}),201
+            return jsonify({"message" : f"Insufficient stock! Available stock: {product.stock}"}),200
+        return jsonify({"message" : f"Product not found!: {name}"}),200
     return render_template('order.html')
 
 if __name__ == '__main__':
